21340a04f1ce2de48ef548d2e6b648384e1f0176
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-06-29 21:03:49 +0630

    add fallback

--- routes/botman.php
+++ routes/botman.php
@@ -8,21 +8,26 @@ use App\Http\Conversations\MenuConversation;
 $botman = resolve('botman');
 
 $botman->hears('hi|hii|hello', function ($bot) {
-    $bot->startConversation(new MenuConversation());
+	$bot->startConversation(new MenuConversation());
 });
 
 $botman->hears('search', function ($bot) {
-    $bot->startConversation(new MarketPriceConversation());
+	$bot->startConversation(new MarketPriceConversation());
 });
 
 $botman->hears('download', function ($bot) {
-    $bot->startConversation(new DownloadApplicationConversation());
+	$bot->startConversation(new DownloadApplicationConversation());
 });
 
 $botman->hears('help', function ($bot) {
-    $bot->startConversation(new HelpConversation());
+	$bot->startConversation(new HelpConversation());
 })->skipsConversation();
 
 $botman->hears('stop', function ($bot) {
-    $bot->reply('ဟုတ်ကဲ့');
+	$bot->reply('ဟုတ်ကဲ့');
 })->stopsConversation();
+
+$botman->fallback(function ($bot) {
+	$bot->reply('Sorry!');
+	$bot->startConversation(new HelpConversation());
+});
