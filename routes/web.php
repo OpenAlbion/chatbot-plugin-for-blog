fe4fd1181ec7661a2cb9719594ec457b5b2e5afa
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-06-29 20:21:30 +0630

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/botman', function () {
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['x.frame.options'])->any('/botman', function () {
     app('botman')->listen();
 });
